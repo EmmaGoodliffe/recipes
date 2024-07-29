bb226258a2783b5862a8d71943ff12657c714085
Emma Goodliffe <emma@piemag.com>
2024-07-29 10:40:55 +0100

    creating custom schema bundler

--- scripts/j2t.ts
+++ scripts/j2t.ts
@@ -3,22 +3,16 @@ import { fetchSchema } from "./fetch";
 import type { ResolverOptions } from "@apidevtools/json-schema-ref-parser";
 import { writeFileSync, readFileSync } from "fs";
 
-const everything = readFileSync('scripts/schema.json').toString();
-
 const read: Extract<ResolverOptions["read"], Function> = async file => {
-  // const name = file.url.slice("schema:".length);
-  // return JSON.stringify(await fetchSchema(name));
-  return everything;
+  const name = file.url.slice("schema:".length);
+  return JSON.stringify(await fetchSchema(name));
 };
 
-const schema = await fetchSchema("Recipe");
-const bundled = await $RefParser.bundle(schema, {
-  resolve: {
-    file: false,
-    http: false,
-    custom: { order: 1, canRead: /^schema:/i, read },
-  },
-});
-
-console.log("WOOHOO BUNDLED!!!");
-writeFileSync("./scripts/Recipe.json", JSON.stringify(bundled));
+writeFileSync(
+  "./scripts/Recipe.json",
+  JSON.stringify(
+    await $RefParser.bundle(await fetchSchema("Recipe"), {
+      resolve: { custom: { order: 1, canRead: /^schema:/i, read } },
+    }),
+  ),
+);
