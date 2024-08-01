135c84f54c1ff49ed7dddaf3570f1d04043401b0
Emma Goodliffe <emma@piemag.com>
2024-08-01 11:31:05 +0100

    massively messed up

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,15 @@
 import type { Actions } from "./$types";
+import { fail } from "@sveltejs/kit";
 
 export const actions = {
   addByUrl: async event => {
     const data = await event.request.formData();
     const url = data.get("url");
+    if (typeof url !== "string") {
+      return fail(400, { message: "no url" });
+    }
+    // const res = await fetch(url);
+    // console.log(res);
     const recipe = { dummy: true };
     return { recipe };
   },
