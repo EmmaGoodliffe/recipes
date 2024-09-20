deda5814dc930c548d817716223ae455ef8636a4
Emma Goodliffe <emma@piemag.com>
2024-09-20 13:22:18 +0100

    ready to use emulators

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
 // import adapter from "@sveltejs/adapter-auto";
-import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "svelte-adapter-firebase";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      //  out: "functions"
-    }),
-  },
+  kit: { adapter: adapter() },
 };
 
 export default config;
